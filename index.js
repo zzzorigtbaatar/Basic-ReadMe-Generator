589cf027f68cf73fcd9d30798d35def0cfbb9827
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-07-18 21:46:26 -0700

    included packages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
