245dc3a045252e7bf9f7ee61d6648fb5a502a18f
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-07-18 22:38:04 -0700

    moved licences to generatemarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -51,4 +51,84 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown;
+const licenses = [{
+    name: 'Apache 2.0',
+    badge: 'https://img.shields.io/badge/License-Apache_2.0-blue.svg',
+    link: 'https://opensource.org/licenses/Apache-2.0'
+},
+{
+    name: 'GNU GPL 3.0',
+    badge: 'https://img.shields.io/badge/License-GPLv3-blue.svg',
+    link: 'https://www.gnu.org/licenses/gpl-3.0'
+},
+{
+    name: 'MIT',
+    badge: 'https://img.shields.io/badge/License-MIT-yellow.svg',
+    link: 'https://opensource.org/licenses/MIT'
+},
+{
+    name: 'BSD 2-Clause',
+    badge: 'https://img.shields.io/badge/License-BSD_2--Clause-orange.svg',
+    link: 'https://opensource.org/licenses/BSD-2-Clause'
+},
+{
+    name: 'BSD 3-Clause',
+    badge: 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg',
+    link: 'https://opensource.org/licenses/BSD-3-Clause'
+},
+{
+    name: 'Boost 1.0',
+    badge: 'https://img.shields.io/badge/License-Boost_1.0-lightblue.svg',
+    link: 'https://www.boost.org/LICENSE_1_0.txt'
+},
+{
+    name: 'CC0 1.0',
+    badge: 'https://licensebuttons.net/l/zero/1.0/80x15.png',
+    link: 'http://creativecommons.org/publicdomain/zero/1.0/'
+},
+{
+    name: 'Eclipse Public 2.0',
+    badge: 'https://img.shields.io/badge/License-EPL_2.0-red.svg',
+    link: 'https://opensource.org/licenses/EPL-2.0'
+},
+{
+    name: 'GNU Affero GPL 3.0',
+    badge: 'https://img.shields.io/badge/License-AGPL_v3-blue.svg',
+    link: 'https://www.gnu.org/licenses/agpl-3.0'
+},
+{
+    name: 'GNU GPL 2.0',
+    badge: 'https://img.shields.io/badge/License-GPL_v2-blue.svg',
+    link: 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html'
+},
+{
+    name: 'GNU Lesser GPL 3.0',
+    badge: 'https://img.shields.io/badge/License-LGPL_v3-blue.svg',
+    link: 'https://www.gnu.org/licenses/lgpl-3.0'
+},
+{
+    name: 'Hippocratic 3.0',
+    badge: 'https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg',
+    link: 'https://firstdonoharm.dev'
+},
+{
+    name: 'Mozilla Public 2.0',
+    badge: 'https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg',
+    link: 'https://opensource.org/licenses/MPL-2.0'
+},
+{
+    name: 'The Unlicense',
+    badge: 'https://img.shields.io/badge/license-Unlicense-blue.svg',
+    link: 'http://unlicense.org/'
+}];
+
+function getLicenseNames() {
+    let licenseNames = [];
+    licenses.forEach((license) => licenseNames.push(license.name))
+    return licenseNames;
+}
+
+module.exports = {
+  generateMarkdown,
+  getLicenseNames
+};
