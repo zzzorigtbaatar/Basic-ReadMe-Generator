42c24ad9552eb35be86d5131f2ee949a10060f94
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-07-18 21:43:03 -0700

    added starter files

---
+++ index.js
@@ -0,0 +1,49 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.appendFile(fileName, data, (err) => 
+    err ? console.error(error) : console.log('README.md created!')
+)
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'Enter your repo name:',
+        name: 'repoName',
+    },
+    {
+        type: 'input',
+        message: 'Enter your repo details',
+        name: 'userDetail'
+    }
+]).then((response) => {
+    let generatedReadMe = `# ${response.repoName}
+
+## Description 
+    
+${response.userDetail}
+
+## Usage
+    
+    
+## Links
+`
+
+    writeToFile('README.md', generatedReadMe)
+
+})
+
+// Function call to initialize app
+init();
